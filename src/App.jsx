f07e911629214e277c9533afb42dc62642c60f54
Farhan Ferdous <farhanferdous@gmail.com>
2023-07-31 05:41:34 +0600

    firebase

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState , useEffect} from "react"
+import { getDatabase, ref, set, push, onValue, remove } from "firebase/database";
+
+function App() {
+
+  const db = getDatabase();
+
+let [values, setValues] = useState({
+  tname: "",
+  tdes: ""
+})
+
+let [taskArr, setTaskArr] = useState([])
+
+let handleChange = (e) => {
+  setValues({
+    ...values,
+    [e.target.name] : e.target.value
+  })
+}
+
+let handleClick = () => {
+  // console.log("farhan")
+  set(push(ref(db, 'firebase')), {
+    taskname: values.tname,
+    description: values.tdes
+  });
+}
+
+useEffect(()=>{
+  const firebaseRef = ref(db, 'firebase');
+  onValue(firebaseRef, (snapshot) => {
+    let arr = []
+    snapshot.forEach((item)=>{
+      arr.push({ ...item.val(), id: item.key})
+    })
+    setTaskArr(arr)
+});
+},[])
+
+// let handleUpdate = ()=>{
+//   console.log("ferdous")
+// }
+
+let handleDelete = (id) =>{
+ remove(ref(db, 'firebase/' + id))
+}
+  
+
+  return (
+    <>
+      <h3 className="fire">REACT WITH FIREBASE</h3>
+     <input name="tname" onChange={handleChange} placeholder="todo" type="text" value={values.tname} />
+     <input name="tdes" onChange={handleChange} placeholder="description" type="text" value={values.tdes} />
+     <button onClick={handleClick}>Click</button>
+     {/* <button onClick={handleUpdate} className="update">Update</button> */}
+
+     <ul>
+     {taskArr.map((item)=>(
+       <>
+       <li>
+        <div className="part">
+        <div className="left">{item.taskname}</div>
+        <div className="right">{item.description}</div>
+        <div className="button">
+        <button className="edit">Edit</button>
+        <button onClick={()=>{handleDelete(item.id)}} className="delete">Delete</button>
+        </div>
+        </div>
+        </li> 
+       </>
+      ))
+     }
+     </ul>
+    </>
+  )
+}
+
+export default App
+
+
---
+++ src/cofig/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnYHRsGePI2ERCKWI4uG50w5pO3_xUtxA",
+  authDomain: "fir-f8bbb.firebaseapp.com",
+  projectId: "fir-f8bbb",
+  storageBucket: "fir-f8bbb.appspot.com",
+  messagingSenderId: "348128590148",
+  appId: "1:348128590148:web:c434f3944f3fd08a1e3f11"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
